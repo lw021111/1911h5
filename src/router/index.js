41f39280faf57be283ccf3a484a013d2a5276a2b
lankaize <2728701189@qq.com>
2020-08-18 08:53:57 +0800

    页面布置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/Index/Index'
+import FullWidth from '@/components/Index/FullWidth'
+import Category from '@/components/Index/Category'
+import News from '@/components/Index/News'
+import Search from '@/components/Index/Search'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Index',
+      component: Index
+    },{
+      path: '/FullWidth',
+      name: 'FullWidth',
+      component: FullWidth
+    },
+    {
+      path: '/Category',
+      name: 'Category',
+      component: Category
+    },{
+      path: '/News',
+      name: 'News',
+      component: News
+    },
+    {
+      path: '/Search',
+      name: 'Search',
+      component: Search
+    },
   ]
 })
